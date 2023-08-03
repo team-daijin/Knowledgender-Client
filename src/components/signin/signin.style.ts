fe59a25da9315438431e63b32ed02f2294a76ebb
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-08-03 14:52:16 +0900

    fix:signin

---
+++ src/components/signin/signin.style.ts
@@ -0,0 +1,150 @@
+import { Placeholder } from "react-bootstrap";
+import styled from "styled-components";
+
+interface InputProps {
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+interface ButtonProps {
+  variant?: "primary" | "secondary";
+  onClick?: () => void;
+}
+export const SigninPageWrap = styled.div`
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SigninWrap = styled.div`
+  width: 60vw;
+  height: 60vh;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputWrap = styled.div`
+  height: 60vh;
+  width: 35vw;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-around;
+`;
+
+export const Input = styled.input<InputProps>`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  outline: none;
+  transition: border-color 0.2s;
+  width: 20vw;
+
+  ::placeholder {
+    color: #aaa;
+  }
+
+  &:focus {
+    border-color: #7b39d1;
+  }
+`;
+export const SigninButton = styled.button<ButtonProps>`
+  padding: 10px 20px;
+  font-size: 16px;
+  font-family: "pretendard";
+  border-radius: 5px;
+  border-style: none;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  width: 21vw;
+  height: 5vh;
+
+  ${({ variant }) => {
+    switch (variant) {
+      case "secondary":
+        return `
+          background-color: #BE8EFC;
+          color: #333;
+        `;
+      default:
+        return `
+          background-color:#BE8EFC;
+          color: #fff;
+        `;
+    }
+  }}
+
+  &:hover {
+    background-color: ${({ variant }) =>
+      variant === "secondary" ? "#BE8EFC" : "#7B39D1"};
+  }
+`;
+
+export const SignupButton = styled.button<ButtonProps>.attrs((place{
+  placeholderTextColor:"#72777a",
+})`
+  padding: 10px 20px;
+  font-size: 16px;
+  font-family: "pretendard";
+  border-radius: 5px;
+  border-style: solid;
+  cursor: pointer;
+  transition: border-color 0.2s;
+  width: 21vw;
+  height: 5vh;
+  background-color: white;
+  color: #72777a;
+
+  ${({ variant }) => {
+    switch (variant) {
+      case "secondary":
+        return `
+          border-color: #DBBFFF;
+            color: #72777a;
+        `;
+      default:
+        return `
+          border-color:#DBBFFF;
+            color: #72777a;
+        `;
+    }
+  }}
+
+  &:hover {
+    border-color: ${({ variant }) =>
+      variant === "secondary" ? "#7B39D1" : "#7B39D1"};
+  }
+`;
+
+export const InputComponent = styled.div`
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+export const InputText = styled.div`
+  font-family: "pretendard";
+  color: #72777a; // 회색
+  font-size: 1.2em;
+`;
+export const InputErrorText = styled.div`
+  font-family: "pretendard";
+  color: #f1281c; // 빨강
+`;
+
+export const ButtonWrap = styled.div`
+  height: 12vh;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`;
--- src/components/signin/signin.tsx
+++ src/components/signin/signin.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import * as S from "./signin.style.tsx";
-
-function Signin() {
+import * as S from "./signin.style.ts";
+import Image from "../../assets/image/signinImg.svg";
+import Logo from "../../assets/image/signinLogo.svg";
+function Signin({ children }: React.PropsWithChildren) {
   return (
     <>
-      <S.SigninWrap>
-        <div>hello</div>
-      </S.SigninWrap>
+      <S.SigninPageWrap>
+        <S.SigninWrap>
+          <S.InputWrap>
+            <img src={Logo} alt="" height={"10%"}></img>
+            <S.InputComponent>
+              <S.InputText>아이디</S.InputText>
+              <S.Input></S.Input>
+              <S.InputErrorText>
+                아이디 또는 비밀번호가 잘못되었습니다.
+              </S.InputErrorText>
+            </S.InputComponent>
+            <S.InputComponent>
+              <S.InputText>비밀번호</S.InputText>
+              <S.Input></S.Input>
+              <S.InputErrorText>
+                아이디 또는 비밀번호가 잘못되었습니다.
+              </S.InputErrorText>
+            </S.InputComponent>
+            <S.ButtonWrap>
+              <S.SigninButton>로그인</S.SigninButton>
+              <S.SignupButton>회원가입</S.SignupButton>
+            </S.ButtonWrap>
+          </S.InputWrap>
+          {/* <S.Image src={Image} alt=""></S.Image> */}
+          <img src={Image} alt=""></img>
+        </S.SigninWrap>
+      </S.SigninPageWrap>
     </>
   );
 }
